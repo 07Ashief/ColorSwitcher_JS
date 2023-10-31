f72ec68c2fbe1f03b3937eef23a54b1b041d97aa
Ashief <07ashief@gmail.com.com>
2023-10-31 19:50:43 +0530

    myprac

---
+++ script2.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body')
+
+// console.log(buttons)
+// console.log(body)
+
+buttons.forEach( function(button){
+    button.addEventListener('click', function(e){
+        let click = e.target.id
+        // console.log(click)
+        if (click === 'grey') {
+            body.style.backgroundColor = click
+        }
+        else if (click === 'white') {
+            body.style.backgroundColor = click
+        }
+        else if (click === 'blue') {
+            body.style.backgroundColor = click
+        }
+        else if (click === 'yellow') {
+            body.style.backgroundColor = click
+        }
+        else if (click === 'clear') {
+            body.style.backgroundColor = 'white'
+        }
+    })
+})
